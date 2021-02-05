22c861d753c151505e08e2ed2916a6d140240815
Dmitry Chaschin <vip131@list.ru>
2021-02-05 17:36:09 +0300

    Initial Commit

---
+++ simpleNumberGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleNumberGenerator
+//
+//  Created by Admin on 05.02.2021.
+//  Copyright © 2021 DmitryChaschin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
