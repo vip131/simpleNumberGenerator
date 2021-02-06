496fd540d687c25bd1f35d119505ad121c0bd13b
Dmitry Chaschin <vip131@list.ru>
2021-02-06 23:10:01 +0300

    uuufff

--- simpleNumberGenerator/Prime.swift
+++ simpleNumberGenerator/Prime.swift
@@ -16,6 +16,7 @@ struct PrimeNumbers {
         var primes = [Int]()
         eratosthenesSieve[0] = false
         eratosthenesSieve[1] = false
+        
         for i in startNum..<upperBound where eratosthenesSieve[i] == true {
             var j = i*i
             while j < upperBound {
@@ -28,6 +29,40 @@ struct PrimeNumbers {
        let primesArr = primes.chunked(into: 2)
         return primesArr
     }
+    
+    
+    
+    static func findPrimeNumberlist(fromNumber:Int, toNumber: Int) -> [[Int]] {
+        var primes = [Int]()
+     for i in (fromNumber + 1)...toNumber
+            {
+                var isPrime = true
+                if i <= 1 { // number must be positive integer
+                    isPrime = false
+                }
+                else if i <= 3 {
+                    isPrime = true
+                }
+                else {
+                    for j in 2...i/2 // here i am using loop from 2 to i/2 because it will reduces the  iteration.
+                    {
+                        if i%j == 0 { // number must have only 1 factor except 1. so use break: no need to check further
+                            isPrime = false
+                            break
+                        }
+                    }
+                }
+                if isPrime  {
+                    primes.append(i)
+                }
+            }
+        let primesArr = primes.chunked(into: 2)
+        return primesArr
+    }
+    
+    
+    
+    
 }
 
 
--- simpleNumberGenerator/ViewController.swift
+++ simpleNumberGenerator/ViewController.swift
@@ -9,14 +9,12 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var prime = PrimeNumbers.generate(upperBound: 100, from: 2)
+    var prime = PrimeNumbers.generate(upperBound: 103, from: 2)
     var scrollIndex = 1
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.prefetchDataSource = self
         tableView.register(UINib(nibName: "Cell", bundle: nil), forCellReuseIdentifier: "cell")
     }
-    
     
     //MARK: - TableView DataSource -
     
@@ -29,32 +27,31 @@ class ViewController: UITableViewController {
         let currentArr = prime[indexPath.row]
         cell.leftLabel.text = String(currentArr[0])
         
-
         if currentArr.count < 2 {
-          cell.rightLabel.text = ""
+            cell.rightLabel.text = ""
         } else {
-        
-        cell.rightLabel.text = String(currentArr[1])
+            cell.rightLabel.text = String(currentArr[1])
         }
         return cell
     }
     
     //MARK: - TableView Delegate
-     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-         return 60
-     }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let cell1 = cell as! Cell
-        if indexPath.row + 1 == prime.count {
+        if indexPath.row == prime.count - 2 {
             scrollIndex += 1
-            let nextPrimeNumArray = PrimeNumbers.generate(upperBound: 100 * scrollIndex, from: indexPath.row * 2)
+            let lastPrimeNum = prime.last?.last
+            let nextPrimeNumArray = PrimeNumbers.findPrimeNumberlist(fromNumber: lastPrimeNum!, toNumber: scrollIndex * 103)
             
             for arrOfPrimeNum in nextPrimeNumArray {
-                prime.append(arrOfPrimeNum)
+                self.prime.append(arrOfPrimeNum)
             }
             tableView.reloadData()
-           }
+        }
         cell1.textLabel?.textAlignment = .center
         let number = Number(isGray: false)
         if (indexPath.row % 2 == 0) {
@@ -62,15 +59,16 @@ class ViewController: UITableViewController {
         }
         cell1.rightView.backgroundColor = number.isGray ? .clear : .lightGray
         cell1.leftView.backgroundColor = number.isGray ? .lightGray : .clear
+        
     }
-    
-    
-    
-
 }
 extension ViewController: UITableViewDataSourcePrefetching {
     
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-        print("prefetch \(indexPaths[0].row)")
+        // print("prefetch \(indexPaths[0].row)")
+        
+        
+        
     }
+    
 }
